811b1d61f57803f03338a91f15c810bfb5e9cb4d
Vuong Anh <93652358+Gnouhk@users.noreply.github.com>
2024-04-29 11:23:48 +0700

    Update CarManager.cs

--- AI Experiment Project/Assets/Scripts/Neural Network/CarManager.cs
+++ AI Experiment Project/Assets/Scripts/Neural Network/CarManager.cs
@@ -2,34 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Resources;
 
 public class CarManager : MonoBehaviour
 {
     public List<AgentMovement> allCars;
+    private int carsCount = 0;
+    private float bestReward = float.MinValue;
+    private (float[,], float[])[] bestWeightsAndBiases;
+
+    private int generationSize = 10;
+    private int currentGenerationCount = 0;
+    private List<float> generationRewards = new List<float>();
+    private List<(float[,], float[])[]> generationWeightsAndBiases = new List<(float[,], float[])[]>();
+
+    private void Start()
+    {
+        bestWeightsAndBiases = null;
+    }
 
     private void Update()
     {
-        UpdateCarWeightsAndBiases();
+        //this only call the update method when 10 cars have been evaluated
+        if(carsCount >= 10)
+        {
+            UpdateCarWeightsAndBiases();
+            carsCount = 0; //reset the counter for the next gen.
+        }
+    }
+    public void OnCarDestroyed (AgentMovement destroyedCar)
+    {
+        currentGenerationCount++;
+        generationRewards.Add(destroyedCar.CalculatedReward());
+        generationWeightsAndBiases.Add(destroyedCar.neuralNetwork.GetWeightsAndBiases());
+
+        if(currentGenerationCount >= generationSize)
+        {
+            int bestIndex = generationReward.IndexOf(generationRewards.Max());
+            (float[,], float[])[] bestWeightsAndBiases = generationWeightsAndBiases[bestIndex];
+        }
     }
 
     private void UpdateCarWeightsAndBiases()
     {
         //find the best performing car based on the reward
-        AgentMovement bestCar = allCars.OrderByDescending(car => car.CalculatedReward()).FirstOrDefault();
-
-        if (bestCar != null)
+        foreach(var car in allCars)
         {
-            //retrive the weights and biases from the best car
-            (float[,], float[]) [] bestWeightsAndBiases = bestCar.neuralNetwork.GetWeightsAndBiases();
-
-            //apply weights and biases to all other cars
-            foreach(var car in allCars)
+            var currentReward = car.CalculatedReward();
+            if(currentReward > bestReward)
             {
-                if(car != bestCar)
-                {
-                    car.neuralNetwork.SetWeightsAndBiases(bestWeightsAndBiases);
-                }
+                bestReward = currentReward;
+                bestWeightsAndBiases = car.neuralNetwork.GetWeightsAndBiases();
             }
+        }
+
+        //apply weights and biases from the best car to all other cars.
+        if(bestWeightsAndBiases != null)
+        {
+            foreach (var car in allCars)
+            {
+                car.neuralNetwork.SetWeightsAndBiases(bestWeightsAndBiases);
+            }
+            //reset the best performance for the next gen
+            bestReward = float.MinValue;
+            bestWeightsAndBiases = null;
         }
     }
 }
