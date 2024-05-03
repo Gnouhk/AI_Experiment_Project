a52a26571bff1d35f5a4a6287206a9cd1761ced9
Vuong Anh <93652358+Gnouhk@users.noreply.github.com>
2024-05-03 15:31:14 +0700

    Starting making evolution manager

--- AI Experiment Project/Assets/Scripts/Deep Learning Car/Evolution/EvolutionManager.cs
+++ AI Experiment Project/Assets/Scripts/Deep Learning Car/Evolution/EvolutionManager.cs
@@ -6,15 +6,76 @@ using System.IO;
 
 public class EvolutionManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Members
+
+    private static System.Random randomizer = new System.Random();
+    public static EvolutionManager Instance { get; private set; }
+
+    // Write down results of each generation to file.
+    [SerializeField]
+    private bool SaveStatistics = false;
+    private string statisticsFileName;
+
+    // Write down the number of first to finished to file
+    [SerializeField]
+    private uint SaveFirstNGenotype = 0;
+    private uint genotypesSaved = 0;
+
+    // Population size
+    [SerializeField]
+    private int PopulationSize = 30;
+
+    // The generic algo restart after a certain amount of generations
+    [SerializeField]
+    private int RestartAfter = 100;
+
+    // Use elitist selection or remainder stochastic sampling
+    [SerializeField]
+    private bool ElitistSelection = false;
+
+    // Topology of the agent's FNN
+    [SerializeField]
+    private uint[] FNNTopology;
+
+    // The current population
+    private List<Agent> agents = new List<Agent>();
+
+    // The amount of agents that currently alive
+    public int AgentsAliveCount { get; private set; }
+
+    // Event when all agents died
+    public event System.Action AllAgentsDied;
+
+    private GenericAlgorithm genericAlgorithm;
+
+    // The age of the current generation
+    public uint GenerationCount
     {
-        
+        get { return genericAlgorithm.GenerationCount; }
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+
+    #region Constructors
+
+    private void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Debug.LogError("More than one EvolutionManager in the Scene"); 
+            return
+        }
+        Instance = this;
     }
+
+    #endregion
+
+    #region Methods
+
+    public void StartEvolution()
+    {
+
+    }
+
+    #endregion
 }
